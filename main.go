0a88c334666edcf26122cce3eec5ecb4a60781b8
BigBricks <bsa6.23.94@gmail.com>
2019-05-10 11:22:16 -0400

    Basic Gorm

--- main.go
+++ main.go
@@ -1,20 +1,28 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
-// type Bro struct {
-// 	ID        uint   `json:"id"`
-// 	FirstName string `json:"firstname"`
-// }
+type Bro struct {
+	ID        uint   `json:"id"`
+	FirstName string `json:"firstname"`
+}
 
 func main() {
 	db, _ := gorm.Open("sqlite3", "./gorm.db")
 	defer db.Close()
-	// p1 := Bro{FirstName: "Bro"}
-	// fmt.Println(p1.FirstName)
+
+	db.AutoMigrate(&Bro{})
+	p1 := Bro{FirstName: "Bro"}
+	db.Create(&p1)
+	var p3 Bro
+	db.First(&p3)
+	fmt.Println(p1.FirstName)
+	fmt.Println(p3.FirstName)
 	// r := gin.Default()
 	// r.GET("/", func(c *gin.Context) {
 	// 	c.JSON(200, gin.H{
