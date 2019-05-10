9733c1380fe022c4c86502c857adc371b560ea6b
BigBricks <bsa6.23.94@gmail.com>
2019-05-10 14:40:30 -0400

    Working

--- main.go
+++ main.go
@@ -8,6 +8,19 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
+func main() {
+	db, err := gorm.Open("sqlite3", "/tmp/gorm.db")
+	defer db.Close()
+	if err != nil {
+		fmt.Println(err)
+	}
+	db.AutoMigrate(&Bro{})
+	r := gin.Default()
+	r.GET("/", GetBros)
+
+	r.Run(":8080")
+}
+
 //Bro comment for the linter to stop yelling
 type Bro struct {
 	ID        uint   `json:"id"`
@@ -15,27 +28,28 @@ type Bro struct {
 	LastName  string `json:"lastname"`
 }
 
-func main() {
-	db, err := gorm.Open("sqlite3", "./gorm.db")
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer db.Close()
-
-	db.AutoMigrate(&Bro{})
-	b1 := Bro{FirstName: "Bro", LastName: "Cephus"}
-	db.Create(&b1)
-	r := gin.Default()
-	r.GET("/", GetBros)
-	r.Run(":8080")
-}
+//dbCon
+// func dbCon() *gorm.DB {
+// 	var db, err = gorm.Open("sqlite3", "./gorm.db")
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
+// 	defer db.Close()
+// 	return db
+// }
 
 //GetBros func
 func GetBros(c *gin.Context) {
 	var bros []Bro
+	db, err := gorm.Open("sqlite3", "/tmp/gorm.db")
+	defer db.Close()
+	if err != nil {
+		fmt.Println(err)
+	}
+	db.Find(&bros)
 	if len(bros) <= 0 {
 		c.AbortWithStatus(404)
-		fmt.Println("No Bros Found, Sorry Bro")
+		fmt.Println("L")
 	} else {
 		c.JSON(200, bros)
 	}
